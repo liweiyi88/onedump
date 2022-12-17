c7938db5d71db9a28c9a3ed53031d9a52b450509
liweiyi88 <weiyi.li713@gmail.com>
2022-12-17 20:47:25 +1100

    refactoring

---
+++ dump/closer.go
@@ -0,0 +1,27 @@
+package dump
+
+import (
+	"io"
+
+	"github.com/hashicorp/go-multierror"
+)
+
+type MultiCloser struct {
+	closers []io.Closer
+}
+
+func NewMultiCloser(closers []io.Closer) *MultiCloser {
+	return &MultiCloser{
+		closers: closers,
+	}
+}
+
+func (m *MultiCloser) Close() error {
+	var err error
+	for _, c := range m.closers {
+		if e := c.Close(); e != nil {
+			err = multierror.Append(err, e)
+		}
+	}
+	return err
+}
