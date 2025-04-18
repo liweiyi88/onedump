fd20a54d63ad3fb4a12777d5cf5c54323338d622
Julian Li <weiyi.li713@gmail.com>
2025-04-18 17:23:01 +1000

    fixe PG version switch

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -e
 
-pg_versions set-default "${PG_VERSION:-16}"
+PG_VERSION=${PG_VERSION:-16}
+
+# Validate input
+if [ "$PG_VERSION" != "15" ] && [ "$PG_VERSION" != "16" ]; then
+  echo "Error: PG_VERSION must be either 15 or 16" >&2
+  exit 1
+fi
+
+# Update symlinks to point to the requested version
+for cmd in psql pg_dump pg_restore pg_dumpall; do
+  ln -sf "/usr/lib/postgresql/$PG_VERSION/bin/$cmd" "/usr/local/bin/$cmd"
+done
 
 onedump "$@"
